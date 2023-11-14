29dbd9678c5040d30e1b64e09acf6d6149474394
roleigh1 <robin.leigh@outlook.de>
2023-11-15 00:34:59 +0100

    shorten the dataFetchController

---
+++ controllers/dataFetchControllers.js
@@ -0,0 +1,43 @@
+const { SeasonCardsDB , BestsellerItemsDB , ProductsDB } = require('../models/models');
+
+const getCardInfo = async (req, res) => {
+    const result = await fetchData(SeasonCardsDB, 4, req.query.page);
+    res.status(200).json(result);
+};
+
+const getBestsellerItems = async (req, res) => {
+    const result = await fetchData(BestsellerItemsDB, 4, req.query.page);
+    res.status(200).json(result);
+};
+
+const getProducts = async (req, res) => {
+    const result = await fetchData(ProductsDB, 19, req.query.page);
+    res.status(200).json(result);
+};
+
+const fetchData = async (db, limit, page) => {
+    const offset = limit * ((parseInt(page) || 1) - 1);
+    try {
+        const { count, rows } = await db.findAndCountAll({
+            limit,
+            offset,
+            order: [['id', 'ASC']],
+        });
+        return {
+            result: rows,
+            count,
+            pages: Math.ceil(count / limit),
+        };
+    } catch (error) {
+        console.error('Error fetching infos: ', error);
+        return 'Internal Server Error';
+    }
+};
+
+
+module.exports = {
+    getCardInfo,
+    getBestsellerItems,
+    getProducts
+
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express'); 
-const dataController = require('../controllers/dataController'); 
+
 const stripeController = require("../controllers/stripeController"); 
-const paginierung = require('../controllers/paginierung');
+const fetchData = require("../controllers/dataFetchControllers");
 const bodyParser = require('body-parser');
 
 const router = express.Router(); 
 
-router.get('/infos', paginierung.getCardInfo);
-router.get('/products',dataController.getProducts);
-router.get('/items' , dataController.getBestsellerItems); 
+router.get('/CardInfos', fetchData.getCardInfo);
+router.get('/Products',fetchData.getProducts);
+router.get('/BestsellerItems' , fetchData.getBestsellerItems); 
 router.post('/create-checkout-session', bodyParser.json(),stripeController.createCheckoutSession);
 router.post('/webhook', express.raw({type: 'application/json'}), stripeController.handleWebhook);
 
